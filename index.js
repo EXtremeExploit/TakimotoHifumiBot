bf842fdd2d0ef7565ec553346b4b96a2efb11fed
EXtremeExploit <gdextremeexploit@gmail.com>
2018-09-05 15:34:54 -0300

    Added code

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+var discord = require('discord.js');
+var client = new discord.Client();
+
+client.on('ready',() => {
+    console.log('Ready!');
+    console.log('aaoao')
+});
+
+client.on('guildMemberAdd', (member) => {
+    var channel = member.guild.channels.find((ch) => ch.id == '485096996047421440');
+    channel.send('W-Welcome to the server '+member+' p-please enjoy your time here <a:HifumiSurprise:486547902660083723>', {
+        file: './welcome.gif'
+    });
+});
+
+client.login(process.env.TOKEN);
