60f5cd4b68ff2e3692593c5caf16c52c0c1f908c
EXtremeExploit <gdextremeexploit@gmail.com>
2018-09-08 13:04:37 -0300

    Added nsfw access

--- index.js
+++ index.js
@@ -2,16 +2,55 @@ require('dotenv').config();
 var discord = require('discord.js');
 var client = new discord.Client();
 
-client.on('ready',() => {
+var prefix = process.env.prefix;
+var ch_arrivals_id = process.env.ch_arrivals_id;
+var ch_nsfw_access_id = process.env.ch_nsfw_access_id;
+var guild_id = process.env.guild_id;
+
+client.on('ready', () => {
     console.log('Ready!');
-    console.log('aaoao')
 });
 
 client.on('guildMemberAdd', (member) => {
-    var channel = member.guild.channels.find((ch) => ch.id == '485083554351546381');
-    channel.send('W-Welcome to the server '+member+' p-please enjoy your time here <a:HifumiSurprise:486547902660083723>', {
+    var channel = member.guild.channels.find((ch) => ch.id == ch_arrivals_id);
+    channel.send('W-Welcome to the server ' + member + ' p-please enjoy your time here <a:HifumiSurprise:486547902660083723>', {
         file: './welcome.gif'
     });
 });
+
+client.on('message', async (msg) => {
+    if (!msg.guild) return;
+    if (!msg.content.startsWith(prefix)) return;
+
+    var messageArray = msg.content.split(' ');
+    var prefix_command = messageArray[0];
+    var command = messageArray[0].replace(prefix, '');
+    var args = messageArray.slice(1).join(' ');
+
+    switch (command) {
+        case 'sendNSFWaccess':
+            client.guilds.find('id', guild_id).channels.find('id', ch_nsfw_access_id).send('Send `.NSFW` to get NSFW permissions, send again if you don\'t want them anymore');
+    }
+})
+
+client.on('message', async (msg) => {
+    if (!msg.guild) return;
+    if (!msg.content.startsWith(prefix)) return;
+
+    var nsfw_role = msg.guild.roles.find('name', 'NSFW 🔞');
+
+    if (msg.channel.id == ch_nsfw_access_id) {
+        if (msg.content == '.NSFW') {
+            if (!msg.member.roles.find('name', 'NSFW 🔞')) {
+                msg.member.addRole(nsfw_role);
+                msg.delete();
+            } else {
+                msg.member.removeRole(nsfw_role);
+                msg.delete();
+            }
+        }
+    }
+})
+
 
 client.login(process.env.TOKEN);
